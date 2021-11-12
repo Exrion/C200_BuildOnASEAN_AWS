78e09de8805a80d8744b60aea85f14ca4ca9e8cc
Titus Lim <20000650@myrp.edu.sg>
2021-11-12 10:11:25 +0800

    Add project files.

---
+++ C200 Web Application/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace C200_Web_Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
