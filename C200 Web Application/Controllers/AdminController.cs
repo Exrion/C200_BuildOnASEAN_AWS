655d83ea310badfa4a371e0ca8e89ac1ba137e66
Titus Lim <20000650@myrp.edu.sg>
2021-11-13 10:42:23 +0800

    Webpage layout template implemented for Admin

--- C200 Web Application/Controllers/AdminController.cs
+++ C200 Web Application/Controllers/AdminController.cs
@@ -10,12 +10,27 @@ namespace C200_Web_Application.Controllers
     {
         public IActionResult Index()
         {
-            return View("Home");
+            return View("Home_Content");
         }
 
         public IActionResult Home()
         {
-            return View("Home");
+            return View("Home_Content");
+        }
+
+        public IActionResult Cameras()
+        {
+            return View("Cameras_Content");
+        }
+
+        public IActionResult Contacts()
+        {
+            return View("Contacts_Content");
+        }
+
+        public IActionResult Analytics()
+        {
+            return View("Analytics_Content");
         }
 
         //TEMP; REMOVE LATER
