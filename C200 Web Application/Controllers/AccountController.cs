0d935f7f7a311206f90a8ba8e5a90e6f178d68ae
Toh YuanWei <02yuanweic2@gmail.com>
2021-11-14 16:42:32 +0800

    Login Page

---
+++ C200 Web Application/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Login.Models;
+using Microsoft.AspNetCore.Http;
+
+
+namespace TestArea.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Verify(Account acc)
+        {
+
+
+            IFormCollection form = HttpContext.Request.Form;
+            String username = form["username"].ToString().Trim();
+            String password = form["password"].ToString().Trim();
+
+            if (ValidUtl.CheckIfEmpty(username, password))
+            {
+                ViewData["Message"] = "Please enter all fields";
+                return View("login");
+
+
+            }
+
+            return View("Home");
+        }
+    }
+}
---
+++ C200 Web Application/Controllers/server.php
@@ -0,0 +1,31 @@
+﻿<?php
+session_start();
+
+$username = "";
+$password = "";
+
+$errors = array();
+
+$db = mysql_connect('localhost','root','','account') or die("could not connect to database");
+
+//form validation
+
+if(empty($username)) {array_push($errors, "Username is required")};
+if(empty($password)) {array_push($errors, "Password is required")};
+
+$sql = "select * from loginform where user='".$username."'AND Password='".$password."' limit 1";
+
+$result=mysql_query($sql);
+
+    if(mysql_num_row($result)==1){
+    ViewData["Message"] = "Logged In";
+    exit();
+    }
+
+    else{
+    ViewData["Message"] = "You have Entered an Incorrect Password";
+    exit();
+    }
+    header('location: login.cshtml');
+?>
+
