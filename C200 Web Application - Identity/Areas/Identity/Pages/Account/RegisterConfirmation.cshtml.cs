1c08cc0b9aaa5d1a3a4ba6fdc4be2b5f73b200d7
Titus Lim <titus.happy@gmail.com>
2021-11-28 17:40:10 +0800

    Register webpage updated to reflect functioning email service

--- C200 Web Application - Identity/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
+++ C200 Web Application - Identity/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
@@ -43,7 +43,7 @@ namespace C200_Web_Application___Identity.Areas.Identity.Pages.Account
 
             Email = email;
             // Once you add a real email sender, you should remove this code that lets you confirm the account
-            DisplayConfirmAccountLink = true;
+            DisplayConfirmAccountLink = false;
             if (DisplayConfirmAccountLink)
             {
                 var userId = await _userManager.GetUserIdAsync(user);
