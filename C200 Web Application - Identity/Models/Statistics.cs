f2f3a2ba5c07659fc4a9eca0f84e9e9dc392f3cd
Toh YuanWei <02yuanweic2@gmail.com>
2022-01-26 15:26:25 +0800

    Test Statistics

---
+++ C200 Web Application - Identity/Models/Statistics.cs
@@ -0,0 +1,20 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace C200_Web_Application___Identity.Models
+{
+    public class Statistics
+    {
+        [Name("Loss")]
+        public double Loss { get; set; }
+        [Name("Valid Loss")]
+        public double ValidLoss { get; set; }
+        [Name("Acc")]
+        public double Accuracy { get; set; }
+        [Name("Valid Acc")]
+        public double ValidAccuracy { get; set; }
+    }
+}
