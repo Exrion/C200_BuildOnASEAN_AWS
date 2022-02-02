f78d9daa4bc49b0c65ed9e95af668fb25937741c
GOHSIMYNGNICOLE <20014867@myrp.edu.sg>
2022-02-02 23:23:46 +0800

    model for Location_has_camera created

---
+++ C200 Web Application - Identity/Models/Location_has_camera.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace C200_Web_Application___Identity.Models
+{
+    public class Location_has_camera
+    {
+        public int Level_no { get; set; }
+       
+        public string Location_Location_id { get; set; }
+
+        public int Organisation_Organisation_id { get; set; }
+
+        public string Users_Id { get; set; }
+
+    }
+}
