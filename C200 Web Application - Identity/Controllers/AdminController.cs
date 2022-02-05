ed451a1862623f25c74b33c16b8400d218284b5b
JeraldLoh <jewerldloh19@outlook.com>
2022-02-05 16:17:06 +0800

    final push -jerald

--- C200 Web Application - Identity/Controllers/AdminController.cs
+++ C200 Web Application - Identity/Controllers/AdminController.cs
@@ -31,10 +31,7 @@ namespace C200_Web_Application___Identity.Controllers
         #region Cameras - VIEW
         public IActionResult Cameras()
         {
-            string sql = "SELECT C.Camera_id, C.Serial_no, C.Location_has_camera_Level_no, L.Location_name, C.Status " +
-                "FROM Camera C " +
-                "INNER JOIN Location_has_camera LC ON C.Location_has_camera_Level_no = LC.Level_no " +
-                "INNER JOIN Location L ON LC.Location_Location_id = L.Location_id";
+            string sql = "SELECT * FROM Camera";
             List<Camera> cameraList = DBUtl.GetList<Camera>(sql);
 
             return View(cameraList);
